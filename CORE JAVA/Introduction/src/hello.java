325bbcc3643420024cb62df63e6aa48be4b38899
Abhishek Samadhiya <abhisheksamadhiya7@gmail.com>
2026-07-17 17:32:08 +0530

    feat: day 1 - Java introduction and first programs

---
+++ CORE JAVA/Introduction/src/hello.java
@@ -0,0 +1,10 @@
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My Name is Abhishek Samadhiya ");
+        System.out.println("I have done my B.tech from Shri Ram Group Of Institutions");
+        System.out.println("My branch is Computer Science and Engineering ");
+
+    }
+}
