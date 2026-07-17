98c7cc161df7789359d984a46f4f1391a7b4d7b9
Abhishek Samadhiya <abhisheksamadhiya7@gmail.com>
2026-07-17 17:48:49 +0530

    feat: day 1 - Java introduction mini project

--- CORE JAVA/Introduction/src/StudentProfile.java
+++ CORE JAVA/Introduction/src/StudentProfile.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+public class StudentProfile
+{
+    public static void main(String args[])
+    {
+        System.out.println("==============================");
 
-public class StudentProfile {
+        System.out.println("     Student Profile     ");
+
+        System.out.println("==============================");
+
+        System.out.println("Name: Abhishek Samadhiya");
+
+        System.out.println(" College : Shri Ram Group of Institutions");
+
+        System.out.println(" Branch : Computer Science and Engineering ");
+
+        System.out.println(" Semester : 6th ");
+
+        System.out.println(" CGPA : 7.83");
+
+        System.out.println(" Skills :-");
+
+        System.out.println(" - Java");
+        System.out.println(" - Spring Boot ");
+        System.out.println(" - Data Structures and Algorithms");
+
+        System.out.println(" Dream Job");
+
+        System.out.println(" Java Backend Engineer");
+    }
 }
