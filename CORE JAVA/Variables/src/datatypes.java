e0e03bb4d0cef460b44fce5a8b55570d35dad2ac
Abhishek Samadhiya <abhisheksamadhiya7@gmail.com>
2026-07-18 22:02:17 +0530

    feat(core-java): complete day 2 - variables, data types and type casting

---
+++ CORE JAVA/Variables/src/datatypes.java
@@ -0,0 +1,24 @@
+public class datatypes
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        double d=23.33;
+        float f = 3.5f;
+        long l=10l;
+        char c='c';
+        String str="abc";
+        Boolean bool=true;
+
+        System.out.println("Integer value: "+num);
+        System.out.println("Double value: "+d);
+        System.out.println("Float value: "+f);
+        System.out.println("Long value: "+l);
+        System.out.println("Character value: "+c);
+        System.out.println("String value: "+str);
+        System.out.println("Boolean value: "+bool);
+
+
+
+    }
+}
