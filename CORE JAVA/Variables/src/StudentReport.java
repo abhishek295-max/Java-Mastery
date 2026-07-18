171d8b016c1a32cdad9d5b020aa9b0301d7bf9f5
Abhishek Samadhiya <abhisheksamadhiya7@gmail.com>
2026-07-18 20:18:49 +0530

    feat(core-java): complete day 2 - variables, data types and type casting

---
+++ CORE JAVA/Variables/src/StudentReport.java
@@ -0,0 +1,16 @@
+public class StudentReport {
+    public static void main(String []args) {
+        System.out.println("=====================================");
+        System.out.println("         Student Report              ");
+        System.out.println("=====================================");
+        int rollno=101,semester=6,marks=92;
+
+        String name="Abhishek Samadhiya";
+
+        System.out.println("Name: "+name);
+        System.out.println("Roll No: "+rollno);
+        System.out.println("Semester: "+semester);
+        System.out.println("Marks: "+marks);
+
+    }
+}
