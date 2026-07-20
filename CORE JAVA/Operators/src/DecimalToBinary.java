88db76cd6dc769fa3a2bbb4ba51d7b99990ff782
Abhishek Samadhiya <abhisheksamadhiya7@gmail.com>
2026-07-20 13:32:17 +0530

    feat(core-java): complete day 3 - operators and expressions

---
+++ CORE JAVA/Operators/src/DecimalToBinary.java
@@ -0,0 +1,22 @@
+public class DecimalToBinary {
+
+    public static void main(String[] args) {
+
+        int decimal = 13;
+        int binary = 0;
+        int place = 1;
+
+        while (decimal > 0) {
+
+            int remainder = decimal % 2;
+
+            binary = binary + (remainder * place);
+
+            place = place * 10;
+
+            decimal = decimal / 2;
+        }
+
+        System.out.println("Binary = " + binary);
+    }
+}
